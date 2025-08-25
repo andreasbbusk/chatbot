c946a7d03b28d8a2dbcec9800fbd15d5c5b3092f
andreasbbusk <buskmikkelsen@icloud.com>
2025-08-25 10:37:17 +0200

    Ability to serve static files added

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const PORT = 3000;
 app.set("view engine", "ejs"); // Set EJS as the templating engine
 app.use(express.urlencoded({ extended: true })); // For parsing application/x-www-form-urlencoded
 
+app.use("/public", express.static("public"));
+
 const names = [];
 
 // GET - render the index page
@@ -14,6 +16,7 @@ app.get("/", (req, res) => {
   res.render("index", { names: names, error: "", name: "", age: "" });
 });
 
+// POST-request - add a name to the names array and render the index page
 app.post("/submit", (req, res) => {
   const name = req.body.name;
   const age = req.body.age;
@@ -26,6 +29,7 @@ app.post("/submit", (req, res) => {
   res.render("index", { name, age, error, names });
 });
 
+// POST-request - clear the names array competely and reset root page
 app.post("/", (req, res) => {
   names.length = 0;
   res.render("index", { names: names, error: "", name: "", age: "" });
