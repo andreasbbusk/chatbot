e8471055ecd245415263f97e5603ad7c1417611c
andreasbbusk <buskmikkelsen@icloud.com>
2025-09-15 08:08:55 +0200

    Added cors to allow frontend client to communicate with backend server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,17 @@
 import express from "express";
 import responses from "./data/responses.js";
+import cors from "cors";
 import { generateBotResponse } from "./services/chatLogic.js";
 import { sanitizeInput, validateChatMessage } from "./utilities/inputHelpers.js";
 
 const app = express();
+
 const PORT = 3000;
 const messages = [];
 
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
 app.use(express.static("client"));
+app.use(cors());
 
 app.get("/api/messages", (req, res) => {
   const totalMessages = messages.length;
